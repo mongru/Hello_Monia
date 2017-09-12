a1606bdb69fcafb30f81250c4ec6ad24db475095
mongru <monika.grubizna@gmail.com>
2017-09-12 17:45:02 +0200

    bare files structure

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded',
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('prod', function(){
+    return gulp.src('scss/style.scss')
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'compressed',
+        }))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
